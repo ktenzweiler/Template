1739317a838759d985a835def47a246940fd4d3b
ktenzweiler <ktenzweiler@gmail.com>
2020-12-14 00:10:37 -0600

    handled enabling and disabling of sign in button

--- app/src/main/java/com/kodingwithkyle/template/authentication/signin/SignInFragment.kt
+++ app/src/main/java/com/kodingwithkyle/template/authentication/signin/SignInFragment.kt
@@ -6,6 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.core.widget.addTextChangedListener
 import com.kodingwithkyle.template.R
 
 class SignInFragment : Fragment() {
@@ -15,16 +18,36 @@ class SignInFragment : Fragment() {
     }
 
     private lateinit var viewModel: SignInViewModel
+    private lateinit var mSignInButton: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.sign_in_fragment, container, false)
+        val view = inflater.inflate(R.layout.sign_in_fragment, container, false)
+
+        mSignInButton = view.findViewById(R.id.sign_in_btn)
+
+        view.findViewById<EditText>(R.id.email_et).addTextChangedListener {
+            it?.let {
+                viewModel.updateEmail(it.trim().toString())
+            }
+        }
+
+        view.findViewById<EditText>(R.id.password_et).addTextChangedListener {
+            it?.let {
+                viewModel.updatePassword(it.trim().toString())
+            }
+        }
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(SignInViewModel::class.java)
+
+        viewModel.isSignInButtonEnabled.observe(viewLifecycleOwner) {
+            mSignInButton.isEnabled = it
+        }
     }
 }
--- app/src/main/java/com/kodingwithkyle/template/authentication/signin/SignInViewModel.kt
+++ app/src/main/java/com/kodingwithkyle/template/authentication/signin/SignInViewModel.kt
@@ -1,7 +1,30 @@
 package com.kodingwithkyle.template.authentication.signin
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class SignInViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    val isSignInButtonEnabled = MutableLiveData(false)
+    private var mEmailText = ""
+    private var mPasswordText = ""
+
+    fun updateEmail(email: String) {
+        mEmailText = email
+        checkSignInButtonEnabled()
+    }
+
+    fun updatePassword(password: String) {
+        mPasswordText = password
+        checkSignInButtonEnabled()
+    }
+
+    private fun checkSignInButtonEnabled() {
+        isSignInButtonEnabled.postValue(
+            mEmailText.contains("@")
+                    and mEmailText.contains(".")
+                    and (mEmailText.length > 5)
+                    and (mPasswordText.length > 5)
+        )
+    }
 }
