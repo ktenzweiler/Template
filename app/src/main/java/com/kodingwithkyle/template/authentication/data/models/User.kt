a103ef66866796d4ee03a9ee558a1e13f0fe4c01
ktenzweiler <ktenzweiler@gmail.com>
2020-12-25 02:05:35 -0600

    added dependencies for room db, added user repo, dao, and the db class

---
+++ app/src/main/java/com/kodingwithkyle/template/authentication/data/models/User.kt
@@ -0,0 +1,11 @@
+package com.kodingwithkyle.template.authentication.data.models
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "users")
+data class User(@PrimaryKey @ColumnInfo(name = "_id") val _id: String,
+                val email: String,
+                val authToken: String?,
+                var isSelf: Boolean = false)
--- app/src/main/java/com/kodingwithkyle/template/authentication/models/User.kt
+++
@@ -1,5 +0,0 @@
-package com.kodingwithkyle.template.authentication.models
-
-data class User(val _id: String,
-                val email: String,
-                val authToken: String?)
