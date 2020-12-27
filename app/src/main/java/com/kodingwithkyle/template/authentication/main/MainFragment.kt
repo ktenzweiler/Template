2efa64c4f196ad78cd1a3fe67bb7640518a0dc42
ktenzweiler <ktenzweiler@gmail.com>
2020-12-27 03:31:16 -0600

    built main screen with mvvm

--- app/src/main/java/com/kodingwithkyle/template/authentication/main/MainFragment.kt
+++ app/src/main/java/com/kodingwithkyle/template/authentication/main/MainFragment.kt
@@ -1,12 +1,22 @@
 package com.kodingwithkyle.template.authentication.main
 
+import android.content.Context
+import android.content.Intent
+import android.net.ConnectivityManager
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.viewModels
 import com.kodingwithkyle.template.R
+import com.kodingwithkyle.template.authentication.AuthenticationActivity
+import com.kodingwithkyle.template.authentication.data.AppDatabase
+import com.kodingwithkyle.template.authentication.data.models.User
+import com.kodingwithkyle.template.authentication.data.repo.UserRepo
 
 class MainFragment : Fragment() {
 
@@ -14,17 +24,43 @@ class MainFragment : Fragment() {
         fun newInstance() = MainFragment()
     }
 
-    private lateinit var viewModel: MainViewModel
+    private val connectivityManager: ConnectivityManager by lazy {
+        requireContext().getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    }
+
+    private val viewModel: MainViewModel by viewModels {
+        MainVMFactory(
+            UserRepo.getInstance(AppDatabase.getInstance(requireContext()).userDao()),
+            connectivityManager
+        )
+    }
+
+    lateinit var mUser : User
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        val view = inflater.inflate(R.layout.main_fragment, container, false)
+        view.findViewById<Button>(R.id.logout_btn).setOnClickListener {
+            viewModel.handleLogoutButtonClick(mUser)
+        }
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        viewModel.self.observe(viewLifecycleOwner) {
+            it?.let {
+                mUser = it
+                view?.findViewById<TextView>(R.id.email_tv)?.text = it.email
+            }
+        }
+        viewModel.shouldNavigateToLogin.observe(viewLifecycleOwner) {
+            if (it) {
+                val intent = Intent(requireContext(), AuthenticationActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
--- app/src/main/java/com/kodingwithkyle/template/authentication/main/MainViewModel.kt
+++ app/src/main/java/com/kodingwithkyle/template/authentication/main/MainViewModel.kt
@@ -1,7 +1,54 @@
 package com.kodingwithkyle.template.authentication.main
 
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.kodingwithkyle.template.authentication.data.models.User
+import com.kodingwithkyle.template.authentication.data.repo.UserRepo
+import com.kodingwithkyle.template.authentication.services.AuthenticationService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class MainViewModel internal constructor(
+    private val userRepo: UserRepo,
+    private val connectivityManager: ConnectivityManager
+) : ViewModel() {
+
+    val self = userRepo.fetchSelf()
+    val shouldNavigateToLogin = MutableLiveData(false)
+
+    fun handleLogoutButtonClick(self: User) {
+        if (isInternetAvailable()) {
+            val service = AuthenticationService.AuthServiceCreator.newService()
+            viewModelScope.launch(Dispatchers.IO) {
+                val response = service.logout(self.authToken!!)
+                if (response.isSuccessful) {
+                    val newSelf = User(self._id, self.email, "", false)
+                    userRepo.insertUser(newSelf)
+                    shouldNavigateToLogin.postValue(true)
+                } else {
+                    // show error message, could not logout
+                }
+            }
+        } else {
+            // show error message, no internet available
+        }
+    }
+
+    private fun isInternetAvailable(): Boolean {
+        var result = false
+        val network = connectivityManager.activeNetwork ?: return false
+        val networkCapabilities =
+            connectivityManager.getNetworkCapabilities(network) ?: return false
+        result = when {
+            networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+            else -> false
+        }
+
+        return result
+    }
 }
